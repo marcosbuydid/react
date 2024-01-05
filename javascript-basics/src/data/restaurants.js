424d73012629a04c89c552df434fe14553da0369
marcos <marcosbuydid@gmail.com>
2024-01-05 18:31:13 -0300

    start adding js basics

---
+++ javascript-basics/src/data/restaurants.js
@@ -0,0 +1,31 @@
+export const restaurants = [
+    {
+        id: 1,
+        name: 'Den',
+        owner: 'Zaiyu Hasegawa'
+    },
+    {
+        id: 2,
+        name: 'Hisa Franko',
+        owner: 'Ana Ros'
+    },
+    {
+        id: 3,
+        name: 'Odette',
+        owner: 'Julien Royer'
+    },
+    {
+        id: 4,
+        name: 'Atelier Crenn',
+        owner: 'Dominique Crenn'
+    },
+    {
+        id: 5,
+        name: 'Noma',
+        owner: 'Rene Redzepi'
+    },
+];
+
+export default {
+    restaurants,
+}
