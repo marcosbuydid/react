b91aad7573f5878cd3bfaa206cac8f1bbc795172
marcos <marcosbuydid@gmail.com>
2024-01-10 17:14:05 -0300

    create basic page with props and styles

--- first-steps/src/main.jsx
+++ first-steps/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App propOne="This is propOne" propTwo="This is propTwo" />
+    </React.StrictMode>
+);
