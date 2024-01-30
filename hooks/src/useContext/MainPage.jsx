ef95ddbe6d5bba702116033caf7394f62c635622
marcos <marcosbuydid@gmail.com>
2024-01-30 16:59:10 -0300

    add navbar and links to navigate between pages

--- hooks/src/useContext/MainPage.jsx
+++ hooks/src/useContext/MainPage.jsx
@@ -1,10 +1,27 @@
+import { Link, Navigate, Route, Routes } from "react-router-dom";
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage';
+import { AboutPage } from './AboutPage';
+import { NotFoundPage } from "./NotFoundPage";
+import { Navbar } from "./Navbar";
 
 export const MainPage = () => {
 
     return (
         <>
-            <h1>Main Page</h1>
+            <Navbar />
             <hr />
+
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="about" element={<AboutPage />} />
+
+                <Route path="/*" element={<NotFoundPage />} />
+
+                {/* Alternative to redirect to specific page */}
+                {/* <Route path="/*" element={<Navigate to="/about" />} /> */}
+            </Routes>
         </>
     )
 }
