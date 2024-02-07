a08ae9abac074bfa6790bc22c82844963017c85d
marcos <marcosbuydid@gmail.com>
2024-02-07 19:52:14 -0300

    install react big calendar and start to configure it

--- time-management/src/management/pages/ManagementPage.jsx
+++ time-management/src/management/pages/ManagementPage.jsx
@@ -1,9 +1,62 @@
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { addHours } from 'date-fns';
+import { localizer } from '../../utils/calendarLocalizer';
 import { Navbar } from "../components/Navbar"
+import { CalendarEvent } from '../components/CalendarEvent';
+import { useState } from 'react';
+
+const user = {
+    id: 324624,
+    name: 'Marcos'
+}
+
+const eventsList = [{
+    title: 'Math test',
+    notes: 'Not forget the calculator',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    user: user,
+}]
 
 export const ManagementPage = () => {
+
+    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+
+    const getEventProps = (event, start, end, isSelected) => {
+
+    }
+
+    const onDoubleClick = (event) => {
+        console.log({ doubleClick: event })
+    }
+
+    const onSelect = (event) => {
+        console.log({ click: event })
+    }
+
+    const onViewChanged = (event) => {
+        localStorage.setItem('lastView', event);
+        setLastView(event)
+    }
+
     return (
         <>
             <Navbar />
+
+            <Calendar
+                localizer={localizer}
+                defaultView={lastView}
+                events={eventsList}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)' }}
+                eventPropGetter={getEventProps}
+                components={{ event: CalendarEvent }}
+                onDoubleClickEvent={onDoubleClick}
+                onSelectEvent={onSelect}
+                onView={onViewChanged}
+            />
         </>
     )
 }
---
+++ time-management/src/utils/calendarLocalizer.js
@@ -0,0 +1,15 @@
+import { format, parse, startOfWeek, getDay } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+import { dateFnsLocalizer } from 'react-big-calendar';
+
+const locales = {
+    'en-US': enUS,
+}
+
+export const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+})
