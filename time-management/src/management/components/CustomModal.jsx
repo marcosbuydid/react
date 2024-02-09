12031006725e77cc43e7fc78b039f0c1baee832f
marcos <marcosbuydid@gmail.com>
2024-02-09 13:19:30 -0300

    create validations on modal form

--- time-management/src/management/components/CustomModal.jsx
+++ time-management/src/management/components/CustomModal.jsx
@@ -1,6 +1,11 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import Modal from 'react-modal';
 import './CustomModal.css';
+import { addHours, differenceInSeconds } from 'date-fns';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 const customStyles = {
     content: {
@@ -17,11 +22,85 @@ Modal.setAppElement('#root');
 
 export const CustomModal = () => {
 
-    const [isVisible, setIsVisible] = useState(true)
+    const [isVisible, setIsVisible] = useState(true);
+
+    const [formSubmitted, setFormSubmitted] = useState(false);
 
     const onCloseModal = () => {
-        console.log('closing modal');
         setIsVisible(false);
+    }
+
+    const [formValues, setFormValues] = useState({
+        title: '',
+        notes: '',
+        startDate: new Date(),
+        endDate: addHours(new Date(), 1),
+    })
+
+    const titleValidationClass = useMemo(() => {
+        if (!formSubmitted) {
+            return '';
+        }
+        return (formValues.title.length > 2)
+            ? 'is-valid'
+            : 'is-invalid';
+
+    }, [formValues.title, formSubmitted])
+
+    const notesValidationClass = useMemo(() => {
+        if (!formSubmitted) {
+            return '';
+        }
+        return (formValues.notes.length > 14)
+            ? 'is-valid'
+            : 'is-invalid';
+
+    }, [formValues.notes, formSubmitted])
+
+    const onInputChange = ({ target }) => {
+        setFormValues({
+            ...formValues,
+            [target.name]: target.value
+        })
+    }
+
+    const onStartDateChange = (date) => {
+        setFormValues({
+            ...formValues,
+            startDate: date
+        })
+    }
+
+    const onEndDateChange = (date) => {
+        setFormValues({
+            ...formValues,
+            endDate: date
+        })
+    }
+
+    const onFormSubmit = (event) => {
+
+        event.preventDefault();
+        setFormSubmitted(true);
+
+        const difference = differenceInSeconds(formValues.endDate, formValues.startDate);
+
+        if (isNaN(difference) || difference <= 0) {
+            Swal.fire('End time not valid', 'Choose another after start time', 'error');
+            return;
+        }
+
+        if (formValues.title.length < 3) {
+            Swal.fire('Title name not found', 'Write a title name of at least 3 chars', 'error');
+            return;
+        }
+
+        if (formValues.notes.length < 15) {
+            Swal.fire('Notes cannot be empty', 'Write a note of at least 15 chars', 'error');
+            return;
+        }
+
+        console.log(formValues)
     }
 
     return (
@@ -36,16 +115,33 @@ export const CustomModal = () => {
         >
             <h1> New Event</h1>
             <hr />
-            <form className="container">
+            <form className="container" onSubmit={onFormSubmit}>
 
                 <div className="form-group mb-2">
                     <label className="mb-2">Date and Time</label>
-                    <input className="form-control" placeholder="Start Date" />
+                    <div>
+                        <DatePicker
+                            selected={formValues.startDate}
+                            onChange={(date) => onStartDateChange(date)}
+                            className="form-control"
+                            dateFormat={"Pp"}
+                            showTimeSelect
+                        />
+                    </div>
+
                 </div>
 
                 <div className="form-group mb-2">
-
-                    <input className="form-control" placeholder="End Date" />
+                    <div>
+                        <DatePicker
+                            minDate={formValues.startDate}
+                            selected={formValues.endDate}
+                            onChange={(date) => onEndDateChange(date)}
+                            className="form-control"
+                            dateFormat={"Pp"}
+                            showTimeSelect
+                        />
+                    </div>
                 </div>
 
                 <hr />
@@ -53,10 +149,12 @@ export const CustomModal = () => {
                     <label className="mb-2">Title and Notes</label>
                     <input
                         type="text"
-                        className="form-control mb-2"
+                        className={`form-control mb-2 ${titleValidationClass}`}
                         placeholder="Event Title"
                         name="title"
                         autoComplete="off"
+                        value={formValues.title}
+                        onChange={onInputChange}
                     />
 
                 </div>
@@ -64,10 +162,12 @@ export const CustomModal = () => {
                 <div className="form-group mb-2">
                     <textarea
                         type="text"
-                        className="form-control"
+                        className={`form-control ${notesValidationClass}`}
                         placeholder="Notes"
                         rows="5"
                         name="notes"
+                        value={formValues.notes}
+                        onChange={onInputChange}
                     ></textarea>
                 </div>
 
